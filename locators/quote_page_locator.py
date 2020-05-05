54a84a9ef1fb8b018f3c7a42216af7116fb4e08e
Zaidtech <mzali@zhcet.ac.in>
2020-05-06 00:57:51 +0530

    Selecting the author dropdown menu

--- locators/quote_page_locator.py
+++ locators/quote_page_locator.py
@@ -2,5 +2,5 @@ class QuotePageLocater:
     QUOTE = 'div.quote '
     AUTHOR_DROPDOWN = "select#author"
     SEARCH_BUTTON = 'input[name = "submit_button"]'
-    
+    #  drop downs are called select in selenium
 
--- pages/quote_page.py
+++ pages/quote_page.py
@@ -1,4 +1,5 @@
-
+from typing import List
+from selenium.webdriver.support.ui import Select
 """
     Will be getting the whole page as an object from the app.py
 
@@ -12,7 +13,7 @@ class QuotePage:
         self.browser = browser
 
     @property
-    def quotes(self):
+    def quotes(self) -> List[QuoteParser] :
         #  created an object of a quote from a complete page and passed into    
         return [
             QuoteParser(e)
@@ -20,6 +21,17 @@ class QuotePage:
                 QuotePageLocater.QUOTE
                 )
         ]
+
+    @property
+    def author_dropdown(self) -> Select :
+         element = self.browser.find_element_by_css_selector(
+             QuotePageLocater.AUTHOR_DROPDOWN
+         ) 
+         return Select(element)
+
+    def select_author(self, author_name:str):
+        self.author_dropdown.select_by_visible_text(author_name)
+
 
 
 
