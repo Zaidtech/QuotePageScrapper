73d4650fc900a98f59672e18e6679726670120f5
Zaidtech <mzali@zhcet.ac.in>
2020-05-06 01:47:59 +0530

    Click the search button method

--- locators/quote_page_locator.py
+++ locators/quote_page_locator.py
@@ -2,5 +2,6 @@ class QuotePageLocater:
     QUOTE = 'div.quote '
     AUTHOR_DROPDOWN = "select#author"
     SEARCH_BUTTON = 'input[name = "submit_button"]'
+    TAG_DROPDOWN = "select#tag"
     #  drop downs are called select in selenium
 
--- pages/quote_page.py
+++ pages/quote_page.py
@@ -1,9 +1,7 @@
 from typing import List
+form selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
-"""
-    Will be getting the whole page as an object from the app.py
 
-"""
 
 from locators.quote_page_locator import QuotePageLocater
 from parsers.quote import QuoteParser
@@ -29,9 +27,42 @@ class QuotePage:
          ) 
          return Select(element)
 
+    @property
+    def tags_dropdown(self) -> Select :
+         element = self.browser.find_element_by_css_selector(
+             QuotePageLocater.TAG_DROPDOWN
+         ) 
+         return Select(element)
+
+    @property 
+    def search_button(self):
+        return self.browser.find_element_by_css_selector(
+            QuotePageLocater.SEARCH_BUTTON
+        
+        )
+
+    def get_available_tags(self) -> List[str]:
+        return [option.text.strip() for option in self.tags_dropdown.options]
+
+    def select_tag(self, tag_name:str):
+        self.tags_dropdown.select_by_visible_text(tag_name)
+    
     def select_author(self, author_name:str):
         self.author_dropdown.select_by_visible_text(author_name)
 
+    
+    def search_for_quotes(self, author_name:str, tag_name:str) -> List[QuoteParser]
+        self.select_author(author_name)
+        try:
+            self.select_tag(tag_name)
+        except NoSuchElementException:
+            raise InavalidForAuthorError(
+            f'Author {author_name} does not have any option with the {tag_name} tag'
+        )    
+    self.search_button.click()
+    return self.quotes
 
+class InavalidForAuthorError(ValueError):
+    pass   
 
 
