23214981679cdbac8b75206f780ce025022e21a9
Zaidtech <mzali@zhcet.ac.in>
2020-05-06 00:02:51 +0530

    Retrieve to request method

--- pages/quote_page.py
+++ pages/quote_page.py
@@ -4,19 +4,16 @@
 
 """
 from bs4 import BeautifulSoup
+
 from locators.quote_page_locator import QuotePageLocater
 from parsers.quote import QuoteParser
 
+
 class QuotePage:
     def __init__(self, page):
-        self.soup = BeautifulSoup(page, 'html.parser')
+        self.soup = BeautifulSoup(page,'html.parser')
 
     @property
     def quotes(self):
-        #  created an object of a quote from a complete page and passed into
-        locator = QuotePageLocater.QUOTE
-        quote_tags = self.soup.select(locator)
-        return [QuoteParser(e) for e in quote_tags]
-
-
-
+        # created an object of a quote from a complete page and passed into
+        return [QuoteParser(e) for e in self.soup.select(QuotePageLocater.QUOTE)]
